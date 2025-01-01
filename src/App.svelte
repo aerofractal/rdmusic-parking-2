a9b97e87e857cfd16140ad61c8418354f47108a6
Noah Ramsey <noah@rdmusic.net>
2025-01-01 02:17:12 -0500

    Added links

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,10 @@
 
   <h1>rdmusic.net</h1>
 
+---> <a href="https://github.com/aerofractal">github</a>
+<br>
+---> <a href="https://blog.rdmusic.net">blog</a>
+<br>
+---> <a href="https://tasks.rdmusic.net">tasks api</a>
+
 </main>
